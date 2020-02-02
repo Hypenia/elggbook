33b43119e91c875f5e1d83504faef88fa02db23a
Dave ONchE <daveonche@gmail.com>
2020-02-02 03:17:18 +0100

    Update for elgg 3.1
    Minor changes

--- views/default/page/elements/sidebar.php
+++ views/default/page/elements/sidebar.php
@@ -6,13 +6,7 @@
  */
 
 // echo elgg_view('page/elements/owner_block', $vars);
-
-// $page_menu = elgg_view_menu('page', ['sort_by' => 'name']);
-// if ($page_menu) {
-// 	echo elgg_view_module('info', '', $page_menu, [
-// 		'class' => 'elgg-page-menu',
-// 	]);
-// }
+// echo elgg_view('page/elements/page_menu', $vars);
 
 echo elgg_view('elggbook/components/ads', $vars);
 
